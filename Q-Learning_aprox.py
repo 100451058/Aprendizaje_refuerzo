e78800985b5c11a12618b11ddacc0502f0504b7d
Aimartxo <100537352@alumnos.uc3m.es>
2024-12-18 11:30:44 +0100

    ultima versión de q-learning

--- Q-Learning_aprox.py
+++ Q-Learning_aprox.py
@@ -15,6 +15,43 @@ pasos_por_episodio = []
 recompensa_por_episodio = []
 valor_politica_optima = []
 
+# class Manager:
+#     def __init__(self, env):
+#         self.env: MazeEnv = env
+#         self.q_table = np.zeros((env.maze_shape[0], env.maze_shape[1], env.maze_shape[0], env.maze_shape[1]))  # Q-table para el manager
+#         self.alpha = 0.1
+#         self.gamma = 0.9
+#         self.epsilon = 0.1
+#         self.end = env._end
+#         self.learned_paths = []
+
+#     def select_goal(self, state):
+#         coins = np.argwhere(self.env._maze == 5).tolist()
+#         if np.random.rand() < self.epsilon:
+#             if len(coins) > 0:
+#                 return coins[np.random.randint(len(coins))]
+#             else:
+#                 return self.env._end
+#         else:
+#             if len(coins)>0:
+#                 if np.random.rand() < self.epsilon*2:
+#                     distances = [sum(abs(np.array(state) - np.array(coin))) for coin in coins]
+#                     return coins[np.argmin(distances)]
+#                 else:
+#                     q_values = [self.q_table[state[0], state[1], coin[0], coin[1]] for coin in coins]
+#                     return coins[np.argmax(q_values)]
+#             else:
+#                 return self.end
+
+#     def update_q_table(self, state, goal, reward, next_state,steps):
+#         # Penalizar la ganancia dependiendo del tiempo sin llegar a un goal
+#         time_penalty = 0.9**steps
+#         reward = reward * time_penalty
+#         #Actualizar la tabla Q
+#         end = [int(item) for item in self.end]
+#         best_next_q = np.max([self.q_table[next_state[0], next_state[1], g[0], g[1]] for g in np.argwhere(self.env._maze == 5).tolist() + [end]])
+#         self.q_table[state[0], state[1], goal[0], goal[1]] += self.alpha * (reward + self.gamma * best_next_q - self.q_table[state[0], state[1], goal[0], goal[1]])
+        
 class Manager:
     def __init__(self, env):
         self.env: MazeEnv = env
@@ -27,9 +64,6 @@ class Manager:
         self.prev_goal = None
 
     def select_goal(self, state):
-        #Actualizar epsilon
-        self.epsilon = self.epsilon *0.99 if self.epsilon > 0.10 else 0.1
-
         coins = np.argwhere(self.env._maze == 5).tolist()
         self.prev_goal = state
         if np.random.rand() < self.epsilon:
@@ -154,7 +188,7 @@ for i in range(episodios_usuario):
             elif event.type == pygame.KEYDOWN:
                 action = get_action_from_key(event.key)
                 if action is not None:
-                    _, reward, done, _, _ = env.step(action,goal)
+                    _, reward, done = env.step(action,goal)
                     next_state = env.get_current_position()
                     reward = 1 if reward<1 else reward
                     worker.update_q_table(state, action, reward, next_state,goal,0)
@@ -195,6 +229,7 @@ for i in tqdm(range(episodios_agente)):
     while not done and movimientos < 500:
         action = worker.select_action(state, goal)
         _, reward, done = env.step(action,goal)
+        reward_acumulado += reward
         next_state = env.get_current_position()
         
         worker.update_q_table(state, action, reward, next_state,goal,movimientos)
@@ -222,59 +257,15 @@ for i in tqdm(range(episodios_agente)):
 
 env.close()
 
-#Probar la eficiencia de la politica
-_ = env.reset()
-state = env.get_current_position()
-comienzo = state
-done = False
-goal = manager.select_goal(state)  # El manager selecciona el objetivo al inicio del episodio
-print(f"Objetivo: {goal}")
+# Resultados del aprendizaje
+print("Porcentaje de episodios completados: ", eginak/episodios_agente)
+
 logros = 0
-while not done:
-    action = worker.select_action(state, goal)
-    _, reward, done = env.step(action,goal)
-    next_state = env.get_current_position()
-    
-    state = next_state
-    env.render()
-    # print(f"Estado: {state}, Recompensa: {reward}, Objetivo: {goal}")
-
-    # Si se alcanza el objetivo, selecciona un nuevo objetivo
-    if np.array_equal(state, goal):
-        goal = manager.select_goal(state)
-    
-    if(done):
-        print("SE LOGRO ENCONTRAR EL CAMINO OPTIMO")
-        logros+=1
-        comienzos_buenos.append(comienzo)
-
-print("Porcentaje de eficiencia de la política: ", 100*(logros/episodios_test))
-
-
-
-# Estadisticas
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(15,5))
-plt.subplot(1,3,1)
-plt.plot(pasos_por_episodio)
-plt.title("Pasos por episodio ("+str(sum(pasos_por_episodio)/episodios_agente)+")")
-plt.subplot(1,3,2)
-plt.plot(eficiencia)
-plt.title("Eficiencia ("+str(sum(eficiencia)/episodios_agente)+")")
-plt.subplot(1,3,3)
-plt.plot(recompensa_por_episodio)
-plt.title("Recompensa por episodio ("+str(sum(recompensa_por_episodio)/episodios_agente)+")")
-plt.show()
-
-
-# Esperar a que el usuario introduzca la techa 'Q'
-input = input("Presiona 'Q' cuando estes listo para ver el agente en acción...")
-# Repetir los pasos anteriores para los estados comienzos buenos
-for comienzo in comienzos_buenos:
-    _ = env.reset(comienzo)
-    done = False
+for i in range(episodios_test):
+    #Probar la eficiencia de la politica
+    _ = env.reset()
     state = env.get_current_position()
+    done = False
     goal = manager.select_goal(state)  # El manager selecciona el objetivo al inicio del episodio
     movimientos = 0
     while not done and movimientos < 300:
@@ -294,5 +285,22 @@ for comienzo in comienzos_buenos:
         if(done):
             print("SE LOGRO ENCONTRAR EL CAMINO OPTIMO")
             logros+=1
-        
-        time.sleep(0.5)
+
+print("Porcentaje de eficiencia de la política: ", 100*(logros/episodios_test))
+
+
+
+# Estadisticas
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(15,5))
+plt.subplot(1,3,1)
+plt.plot(pasos_por_episodio)
+plt.title("Pasos por episodio ("+str(sum(pasos_por_episodio)/episodios_agente)+")")
+plt.subplot(1,3,2)
+plt.plot(eficiencia)
+plt.title("Eficiencia ("+str(sum(eficiencia)/episodios_agente)+")")
+plt.subplot(1,3,3)
+plt.plot(recompensa_por_episodio)
+plt.title("Recompensa por episodio ("+str(sum(recompensa_por_episodio)/episodios_agente)+")")
+plt.show()
