6f9aee698085a68de28b4da0641c7f8b54418ed1
Aimartxo <100537352@alumnos.uc3m.es>
2024-12-18 11:26:43 +0100

    ultima versión de q-learning

--- Q-Learning_aprox.py
+++ Q-Learning_aprox.py
@@ -1,22 +1,37 @@
 import numpy as np
 from src.maze import MazeEnv
 import pygame
+from tqdm import tqdm
+import time
 
 # Parámetros
-episodios_usuario = 5
-episodios_agente = 2000
+episodios_usuario = 3
+episodios_agente = 1000
+episodios_test = 10
+
+#Parametros estadisticos
+eficiencia = []
+pasos_por_episodio = []
+recompensa_por_episodio = []
+valor_politica_optima = []
 
 class Manager:
     def __init__(self, env):
         self.env: MazeEnv = env
-        self.q_table = np.zeros((env.maze_shape[0], env.maze_shape[1], env.maze_shape[0], env.maze_shape[1]))  # Q-table para el manager
+        self.q_table = np.ones((env.maze_shape[0], env.maze_shape[1], env.maze_shape[0], env.maze_shape[1]))  # Q-table para el manager
         self.alpha = 0.1
         self.gamma = 0.9
         self.epsilon = 0.1
         self.end = env._end
+        self.learned_paths = []
+        self.prev_goal = None
 
     def select_goal(self, state):
+        #Actualizar epsilon
+        self.epsilon = self.epsilon *0.99 if self.epsilon > 0.10 else 0.1
+
         coins = np.argwhere(self.env._maze == 5).tolist()
+        self.prev_goal = state
         if np.random.rand() < self.epsilon:
             if len(coins) > 0:
                 return coins[np.random.randint(len(coins))]
@@ -24,15 +39,21 @@ class Manager:
                 return self.env._end
         else:
             if len(coins)>0:
+                #Elige la mejor opción
                 q_values = [self.q_table[state[0], state[1], coin[0], coin[1]] for coin in coins]
                 return coins[np.argmax(q_values)]
             else:
                 return self.end
+        
 
-    def update_q_table(self, state, goal, reward, next_state):
-        end = [int(item) for item in self.end]
-        best_next_q = np.max([self.q_table[next_state[0], next_state[1], g[0], g[1]] for g in np.argwhere(self.env._maze == 5).tolist() + [end]])
-        self.q_table[state[0], state[1], goal[0], goal[1]] += self.alpha * (reward + self.gamma * best_next_q - self.q_table[state[0], state[1], goal[0], goal[1]])
+    def update_q_table(self, state, reward,steps):
+        # Penalizar la ganancia dependiendo del tiempo sin llegar a un goal
+        time_penalty = 0.9**steps
+        reward = reward * time_penalty
+        #Actualizar la tabla Q
+        self.q_table[self.prev_goal[0], self.prev_goal[1], state[0], state[1]] = reward
+
+
 
 class Worker:
     def __init__(self, env):
@@ -51,7 +72,11 @@ class Worker:
             q_table = self.q_tables[tuple(goal)]
             return np.argmax(q_table[state[0], state[1]])
 
-    def update_q_table(self, state, action, reward, next_state, goal):
+    def update_q_table(self, state, action, reward, next_state, goal,steps):
+        # Penalizar la ganancia dependiendo del tiempo sin llegar a un goal
+        time_penalty = 0.95**steps
+        reward = reward * time_penalty
+
         q_table = self.q_tables[tuple(goal)]
         best_next_q = np.max(q_table[next_state[0], next_state[1]])
         q_table[state[0], state[1], action] += self.alpha * (reward + self.gamma * best_next_q - q_table[state[0], state[1], action])
@@ -92,10 +117,6 @@ class Worker:
                 # Añadir nuevo objetivo
                 self.learned_paths.append(goal)
 
-
-        
-
-
 # Función para mapear las teclas a acciones
 def get_action_from_key(key):
     if key == pygame.K_RIGHT:
@@ -110,7 +131,7 @@ def get_action_from_key(key):
 
 print("mazed")
 
-env = MazeEnv((10, 10), False, 5, 'human')
+env = MazeEnv((10, 10), False, 3, 'human')
 manager = Manager(env)
 worker = Worker(env)
 
@@ -124,7 +145,7 @@ for i in range(episodios_usuario):
     goal  = manager.select_goal(state)  # El manager selecciona el objetivo al inicio del episodio
     worker.transfer_learning(goal)
     print(f"user episode: {i+1:02d}/{episodios_usuario:02d} -> goal: {goal}")
-    
+    movimientos = 0
     env.render()
     while not done:
         for event in pygame.event.get():
@@ -136,14 +157,15 @@ for i in range(episodios_usuario):
                     _, reward, done = env.step(action,goal)
                     next_state = env.get_current_position()
                     reward = 1 if reward<1 else reward
-                    worker.update_q_table(state, action, reward, next_state,goal)
-                    manager.update_q_table(state, goal, reward, next_state)
+                    worker.update_q_table(state, action, reward, next_state,goal,0)
                     state = next_state
+
+                    movimientos+=1
 
                     # Si se alcanza el objetivo, el episodio termina
                     if np.array_equal(state, goal):
                         goal = manager.select_goal(state)
-                        manager.update_q_table(state, goal, reward, next_state)
+                        manager.update_q_table(state, reward,0)
                         worker.transfer_learning(goal)
                         print(f"user episode: {i+1:02d}/{episodios_usuario:02d} -> goal: {goal}")
         
@@ -154,62 +176,134 @@ env.close()
 
 # ITERACION DEL AGENTE
 print("EL AGENTE SOLO...")
-_ = env.reset()
-state = env.get_current_position()
-done = False
-goal = manager.select_goal(state)  # El manager selecciona el objetivo al inicio del episodio
-env.render()
-
 # Iteración por episodios
-for i in range(episodios_agente):
+# env._place_coins(env._maze, 3)
+for i in tqdm(range(episodios_agente)):
     _ = env.reset()
     state = env.get_current_position()
     done = False
     goal = manager.select_goal(state)  # El manager selecciona el objetivo al inicio del episodio
     worker.transfer_learning(goal)
+    manager.gamma = manager.gamma*0.99 if manager.gamma > 0.4 else 0.4
+    
+    #Variables estadisticas
     movimientos = 0
+    eginak = 0
+    reward_acumulado = 0
 
     # Iteración por pasos
-    while not done and movimientos < 150:
+    while not done and movimientos < 500:
         action = worker.select_action(state, goal)
         _, reward, done = env.step(action,goal)
+        reward_acumulado += reward
         next_state = env.get_current_position()
         
-        worker.update_q_table(state, action, reward, next_state,goal)
-        manager.update_q_table(state, goal, reward, next_state)
+        worker.update_q_table(state, action, reward, next_state,goal,movimientos)
         state = next_state
         env.render()
         movimientos+=1
         # Si se alcanza el objetivo, selecciona un nuevo objetivo
         if np.array_equal(state, goal):
             goal = manager.select_goal(state)
-            manager.update_q_table(state, goal, reward, next_state)
+            manager.update_q_table(state, reward,movimientos)
             worker.transfer_learning(goal)
+            eginak+=1
+        
+        if(done):
+            eginak+=1
+    
+    pasos_por_episodio.append(movimientos)
+    eficiencia.append(eginak)
+    recompensa_por_episodio.append(reward_acumulado)
+
+    
+
+
+    time.sleep(0.01)
 
 env.close()
 
-#Probar la eficiencia de la politica
-_ = env.reset()
-state = env.get_current_position()
-done = False
-goal = manager.select_goal(state)  # El manager selecciona el objetivo al inicio del episodio
-print(f"Objetivo: {goal}")
-while not done:
-    action = worker.select_action(state, goal)
-    _, reward, done = env.step(action,goal)
-    next_state = env.get_current_position()
-    
-    state = next_state
-    env.render()
-    # print(f"Estado: {state}, Recompensa: {reward}, Objetivo: {goal}")
+# Resultados del aprendizaje
+print("Porcentaje de episodios completados: ", eginak/episodios_agente)
 
-    # Si se alcanza el objetivo, selecciona un nuevo objetivo
-    if np.array_equal(state, goal):
-        print(f"Objetivo: {goal}")
-        goal = manager.select_goal(state)
-        manager.update_q_table(state, goal, reward, next_state)
-    
-    if(done):
-        print("SE LOGRO ENCONTRAR EL CAMINO OPTIMO")
+logros = 0
+comienzos_buenos = []
+worker.epsilon = 0
+for i in range(episodios_test):
+    #Probar la eficiencia de la politica
+    if(np.random.rand() < 0.5):
+        _ = env.reset()
+    else:
+        _ = env.reset((1,1))
+    state = env.get_current_position()
+    comienzo = state
+    done = False
+    goal = manager.select_goal(state)  # El manager selecciona el objetivo al inicio del episodio
+    movimientos = 0
+    while not done and movimientos < 300:
+        action = worker.select_action(state, goal)
+        _, reward, done = env.step(action,goal)
+        next_state = env.get_current_position()
+        
+        state = next_state
+        env.render()
+
+        movimientos+=1
+
+        # Si se alcanza el objetivo, selecciona un nuevo objetivo
+        if np.array_equal(state, goal):
+            goal = manager.select_goal(state)
+        
+        if(done):
+            print("SE LOGRO ENCONTRAR EL CAMINO OPTIMO")
+            logros+=1
+            comienzos_buenos.append(comienzo)
+
+print("Porcentaje de eficiencia de la política: ", 100*(logros/episodios_test))
 
 
+
+# Estadisticas
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(15,5))
+plt.subplot(1,3,1)
+plt.plot(pasos_por_episodio)
+plt.title("Pasos por episodio ("+str(sum(pasos_por_episodio)/episodios_agente)+")")
+plt.subplot(1,3,2)
+plt.plot(eficiencia)
+plt.title("Eficiencia ("+str(sum(eficiencia)/episodios_agente)+")")
+plt.subplot(1,3,3)
+plt.plot(recompensa_por_episodio)
+plt.title("Recompensa por episodio ("+str(sum(recompensa_por_episodio)/episodios_agente)+")")
+plt.show()
+
+
+# Esperar a que el usuario introduzca la techa 'Q'
+input = input("Presiona 'Q' cuando estes listo para ver el agente en acción...")
+# Repetir los pasos anteriores para los estados comienzos buenos
+for comienzo in comienzos_buenos:
+    _ = env.reset(comienzo)
+    done = False
+    state = env.get_current_position()
+    goal = manager.select_goal(state)  # El manager selecciona el objetivo al inicio del episodio
+    movimientos = 0
+    while not done and movimientos < 300:
+        action = worker.select_action(state, goal)
+        _, reward, done = env.step(action,goal)
+        next_state = env.get_current_position()
+        
+        state = next_state
+        env.render()
+
+        movimientos+=1
+
+        # Si se alcanza el objetivo, selecciona un nuevo objetivo
+        if np.array_equal(state, goal):
+            goal = manager.select_goal(state)
+        
+        if(done):
+            print("SE LOGRO ENCONTRAR EL CAMINO OPTIMO")
+            logros+=1
+        
+        time.sleep(0.5)
